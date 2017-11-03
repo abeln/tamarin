30e23666d66d38c697932f3b660223bdbd9b00c3
Abel Nieto <anietoro@uwaterloo.ca>
2017-11-03 15:13:08 -0400

    Style fix

--- src/main/scala/com/github/abeln/tamarin/TraceMap.scala
+++ src/main/scala/com/github/abeln/tamarin/TraceMap.scala
@@ -6,13 +6,14 @@ import com.github.abeln.tamarin.SymInstr.{Instr, Trace}
   * Maps traces to other traces
   */
 abstract class TraceMap {
+  import TraceMap.instrToTrace
 
   protected def transform: PartialFunction[Instr, Trace]
 
   def apply(trace: Trace): Trace = {
     trace.flatMap { instr =>
       if (transform.isDefinedAt(instr)) transform(instr)
-      else Seq(instr)
+      else instr
     }
   }
 }
