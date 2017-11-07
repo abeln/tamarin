b3a5f02e4e02436e760f6523b59a6688729e8ca5
Abel Nieto <abeln@google.com>
2017-11-07 18:55:56 -0500

    Add support for memory ops

--- src/main/scala/com/github/abeln/tamarin/Main.scala
+++ src/main/scala/com/github/abeln/tamarin/Main.scala
@@ -11,9 +11,10 @@ object Main {
     val proc = Desugar andThen SSAConv
 
     println(Query.solve(proc(trace(
-      Add(Reg(3), Reg(1), Reg(2)),
-      EqCond(Reg(3), Lit(42)),
-      EqCond(Reg(2), Lit(100))
+      Add(Reg(4), Lit(42), Lit(0)),
+      Sw(Reg(4), 0, Lit(100)),
+      Lw(Reg(3), 50, Reg(1)),
+      EqCond(Reg(3), Lit(42))
     ))))
   }
 }
